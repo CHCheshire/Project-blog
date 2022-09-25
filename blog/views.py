08e5e93a0057cab77e6a066d46f819dea4dd77ed
CHCheshire <pringl31@gmail.com>
2022-09-25 20:59:41 +0000

    Add contact page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Post 
+from .models import Post
 from .forms import CommentForm
 
 
@@ -75,4 +75,3 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-        
